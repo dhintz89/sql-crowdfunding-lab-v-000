a2ea35f6cf252b79d91c393d76b4993ff0bf17ac
Dan Hintz <dhintz@live.com>
2018-10-31 01:51:12 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,37 +1,37 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
-  ("Need a New Car", "Daily Needs", 10000, 1/1/2018, 12/31/2018),
+  ("Need a New Car", "Personal", 10000, 1/1/2018, 12/31/2018),
   ("Code Class", "Education", 10000, 8/1/2018, 7/31/2019),
   ("Career Coaching", "Education", 2000, 6/1/2019, 7/31/2019),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  ("");
+  ("Cruise Fund", "Personal", 5000, 7/1/2018, 9/30/2018),
+  ("Surgery", "Medical", 35000, 6/30/2018, 6/30/2019),
+  ("Backpacking Trip", "Personal", 8000, 10/31/2018, 1/1/2019),
+  ("Broken Leg Repair", "Medical", 5000, 11/01/2018, 12/31/2018),
+  ("Funding for MBA", "Education", 100000, 6/30/2018, 12/31/2019),
+  ("Dream Concert", "Personal", 250, 11/01/2018, 11/15/2018),
+  ("Lasik", "Medical", 2000, 1/1/2019, 6/30/2019);
   
   
 INSERT INTO users (name, age) VALUES
   ("John", 30),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
+  ("April", 29),
+  ("Dan", 29),
+  ("Matt", 42),
+  ("Jake", 31),
+  ("Jenny", 24),
+  ("Melinda", 18),
+  ("Larry", 22),
+  ("Rachel", 36),
+  ("Harry", 50),
   ("Adam", 32)
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  (""),
-  ("");
+  ("Wanda", 46),
+  ("Brian", 40),
+  ("Katie", 19),
+  ("Chad", 21),
+  ("Al", 58),
+  ("Gertrude", 65),
+  ("Donna", 34),
+  ("James", 28),
+  ("Mike", 43);
   
   
 INSERT INTO pledges (amount, user, project) VALUES
@@ -64,4 +64,4 @@ INSERT INTO pledges (amount, user, project) VALUES
   (719,17,4),
   (946,15,2),
   (522,5,4),
-  (426,18,);
+  (426,18,6);
