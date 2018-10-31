f430bd67c169dde7598ca99c76c3459600b722c8
Dan Hintz <dhintz@live.com>
2018-10-31 01:55:10 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,13 +1,13 @@
-INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Need a New Car", "Personal", 10000, 1/1/2018, 12/31/2018),
-  ("Code Class", "Education", 10000, 8/1/2018, 7/31/2019),
-  ("Career Coaching", "Education", 2000, 6/1/2019, 7/31/2019),
-  ("Cruise Fund", "Personal", 5000, 7/1/2018, 9/30/2018),
-  ("Surgery", "Medical", 35000, 6/30/2018, 6/30/2019),
-  ("Backpacking Trip", "Personal", 8000, 10/31/2018, 1/1/2019),
-  ("Broken Leg Repair", "Medical", 5000, 11/01/2018, 12/31/2018),
-  ("Funding for MBA", "Education", 100000, 6/30/2018, 12/31/2019),
-  ("Dream Concert", "Personal", 250, 11/01/2018, 11/15/2018),
-  ("Lasik", "Medical", 2000, 1/1/2019, 6/30/2019);
+INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES ("Need a New Car", "Personal", 10000, 1/1/2018, 12/31/2018);
+  -- ("Code Class", "Education", 10000, 8/1/2018, 7/31/2019),
+  -- ("Career Coaching", "Education", 2000, 6/1/2019, 7/31/2019),
+  -- ("Cruise Fund", "Personal", 5000, 7/1/2018, 9/30/2018),
+  -- ("Surgery", "Medical", 35000, 6/30/2018, 6/30/2019),
+  -- ("Backpacking Trip", "Personal", 8000, 10/31/2018, 1/1/2019),
+  -- ("Broken Leg Repair", "Medical", 5000, 11/01/2018, 12/31/2018),
+  -- ("Funding for MBA", "Education", 100000, 6/30/2018, 12/31/2019),
+  -- ("Dream Concert", "Personal", 250, 11/01/2018, 11/15/2018),
+  -- ("Lasik", "Medical", 2000, 1/1/2019, 6/30/2019);
   
   
 INSERT INTO users (name, age) VALUES
