8401766b7ef0584fae21cfd00c5944a81075ec4b
Dan Hintz <dhintz@live.com>
2018-10-31 01:22:22 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,2 +1,11 @@
 INSERT INTO projects (title, category, funding_goal, start_date, end_date) VALUES 
   ()
+  
+  
+  
+INSERT INTO users (name, age) VALUES
+  ()
+  
+  
+INSERT INTO pledges (amount, user, project) VALUES
+  ()
